234ed455f67c2644dfe39aad810496dc89c962bf
Cristopher Vanegas <cristopher.platzi@gmail.com>
2026-05-17 17:39:54 -0500

    ADD: me quede en el minuto 37:29

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framewor
 from rest_framework import routers
 from tasks import views
 
@@ -11,5 +12,6 @@ router.register(r'tasks', views.TaskView, 'tasks')
 # esto ya me permite consultar peticiones GET, POST, PUT, PATCH, DELETE, ETC
 urlpatterns = [
   path('api/v1/', include(router.urls))
+  path('docs/', include)
 ]
 
