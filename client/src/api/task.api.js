2494e83f15aee6737f491d87dcd9995494ec0157
Cristopher Vanegas <cristopher.platzi@gmail.com>
2026-05-18 14:30:35 -0500

    Optimized query

--- client/src/api/task.api.js
+++ client/src/api/task.api.js
@@ -1,14 +1,9 @@
 import axios from "axios";
 
 const taskApi = axios.create({
-  baseURL:'http://localhost:8000/tasks/api/v1/tasks/'
-})
-export const getAllTasks = () => {
-  // return axios.get("http://localhost:8000/tasks/api/v1/tasks/");
-  return taskApi.get('/')
-};
+  baseURL: "http://localhost:8000/tasks/api/v1/tasks/",
+});
 
-export const createTask = (task) => {
-  // return axios.post("http://localhost:8000/tasks/api/v1/tasks/");
-  return taskApi.post('/', task)
-};
+export const getAllTasks = () => taskApi.get("/");
+
+export const createTask = (task) => taskApi.post("/", task);
