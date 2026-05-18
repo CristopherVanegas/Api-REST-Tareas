46376bf431580a5b773e55fece6982b1639e64e3
Cristopher Vanegas <cristopher.platzi@gmail.com>
2026-05-18 14:29:12 -0500

    [ADD][CV]: Added post method functionality

--- client/src/api/task.api.js
+++ client/src/api/task.api.js
@@ -1,5 +1,14 @@
-import axios from 'axios'
+import axios from "axios";
 
+const taskApi = axios.create({
+  baseURL:'http://localhost:8000/tasks/api/v1/tasks/'
+})
 export const getAllTasks = () => {
-  return axios.get('http://localhost:8000/tasks/api/v1/tasks/')
-}
+  // return axios.get("http://localhost:8000/tasks/api/v1/tasks/");
+  return taskApi.get('/')
+};
+
+export const createTask = (task) => {
+  // return axios.post("http://localhost:8000/tasks/api/v1/tasks/");
+  return taskApi.post('/', task)
+};
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,7 +1,39 @@
+import { useForm } from "react-hook-form";
+import { createTask } from "../api/task.api";
+
+
 export function TaskFormPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = handleSubmit(async (data) => {
+    // console.log(data);
+    const res = await createTask(data)
+    console.log(res)
+  });
+
   return (
-    <div>TaskFormPage</div>
-  )
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          {...register("title", { required: true })}
+        />
+        {errors.title && <span>Title field is required!</span>}
+        <textarea
+          rows="3"
+          id="Description"
+          {...register("description", { required: true })}
+        />
+        {errors.description && <span>Description field is required!</span>}
+        <button>Save</button>
+      </form>
+    </div>
+  );
 }
 
-export default TaskFormPage
+export default TaskFormPage;
