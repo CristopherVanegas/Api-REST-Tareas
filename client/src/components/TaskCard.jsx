5a5d923cd9940321097804ca74e08b0d850b779b
Cristopher Vanegas <cristopher.platzi@gmail.com>
2026-05-19 08:47:34 -0500

    [ADD][CV]: Added break work for cards, in order to make it more responsive

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -8,8 +8,8 @@ export function TaskCard({ task }) {
       className="bg-zinc-800 p-3 hover:bg-amber-700 hover:cursor-pointer"
       onClick={() => navigate(`/tasks/${task.id}/`)}
     >
-      <h1 className="font-bold uppercase">{task.title}</h1>
-      <p className="text-slate-400">{task.description}</p>
+      <h1 className="font-bold uppercase wrap-break-word">{task.title}</h1>
+      <p className="text-slate-400 wrap-break-word">{task.description}</p>
     </div>
   );
 }
